84f5ee893131b6c68dce0848c3483855390f6dc8
itc-c21026 <84373723+itc-c21026@users.noreply.github.com>
2022-06-30 16:13:58 +0900

    Update Investigate.cs

--- Disturbance Suvival/Assets/Scripts/Investigate.cs
+++ Disturbance Suvival/Assets/Scripts/Investigate.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/*------------------------------------
+調べる行為のシステムのプログラム
+------------------------------------*/
+
 public class Investigate : MonoBehaviour
 {
 
